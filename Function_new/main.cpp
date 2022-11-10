62fe9ced779b1afce3856530f6e9cbd1098d58f9
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2022-11-10 16:58:16 +0300

    two

--- Function_new/main.cpp
+++ Function_new/main.cpp
@@ -95,7 +95,7 @@ int number_of_shifts;
 
 void shiftLeft(int arr[], const int n)
 {
-	
+
 	int number_of_shifts;
 	for (int i = 0; i < number_of_shifts;i++)
 	{
@@ -107,10 +107,6 @@ void shiftLeft(int arr[], const int n)
 		arr[n - 1] = buffer;
 	}
 	
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
+	
 
 }
