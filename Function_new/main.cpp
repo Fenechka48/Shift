d75eef127ff6bd13a10d4efeba9a2820ed79c14d
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2022-11-23 10:10:14 +0300

    HOME_ARRAY

--- Function_new/main.cpp
+++ Function_new/main.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
 using namespace std;
+#define delimiter "\n-----------------------------------------------\n"
 
-void FillRand(int arr[], const int n,int minRand=0,int maxRand=100);
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(double arr[], const int n, double minRand = 0, double maxRand = 100);
+
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+
 int Sum(int arr[], int n);
 double  Avg(int arr[], const int n);
 int minValueIn(int arr[], const int n);
 int maxValueIn(int arr[], const int n);
-void shiftLeft(int arr[], const int n);
+void shiftLeft(int arr[], const int n, int number_of_shifts);
+void shiftRight(int arr[], const int n, int number_of_shifts);
+void Sort(int arr[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 5;
-	int arr[n] = {};
-	int number_of_shifts;
+	const int n = 10;
+	int arr[n] = { 0,1,2,3,4,5,6,7,8,9 };
+
 
 	FillRand(arr, n);
 	Print(arr, n);
-	
+
 	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
 	cout << "Среднее арифметическое значение  " << Avg(arr, n) << endl;
 
 	cout << "Минимальное значение из массива: " << minValueIn(arr, n) << endl;
 	cout << "Максимальное значение из массива: " << maxValueIn(arr, n) << endl;
-	
-	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
-	shiftLeft(arr, n);
+	Sort(arr, n);
 	Print(arr, n);
+	//int number_of_shifts;
+	//cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	//shiftLeft(arr, n, number_of_shifts);
+	//Print(arr, n);
+	//shiftRight(arr, n, number_of_shifts);
+	//Print(arr, n);
+
+	cout << delimiter << endl;
+	const int SIZE = 8;
+	double brr[SIZE];
+	FillRand(brr, SIZE);
+	Print(brr, SIZE);
+	//cout << "Сумма элементов массива: " << Sum(brr, SIZE) << endl;
+	//cout << "Среднее арифметическое значение  " << Avg(brr, SIZE) << endl;
+	//cout << "Минимальное значение из массива: " << minValueIn(brr, SIZE) << endl;
+	//cout << "Максимальное значение из массива: " << maxValueIn(brr, SIZE) << endl;
+
 }
 
 void FillRand(int arr[], const int n, int minRand, int maxRand)
@@ -42,15 +64,16 @@ void FillRand(int arr[], const int n, int minRand, int maxRand)
 	if (minRand == maxRand)maxRand++;
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % (maxRand-minRand)+minRand;
+		arr[i] = rand() % (maxRand - minRand) + minRand;
 	}
 }
 void Print(int arr[], const int n)
 {
-	for (int i = 0; i< n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
+	cout << endl;
 }
 int Sum(int arr[], int n)
 {
@@ -64,7 +87,7 @@ int Sum(int arr[], int n)
 }
 double Avg(int arr[], const int n)
 {
-	return (double) Sum(arr, n) / n;
+	return (double)Sum(arr, n) / n;
 }
 
 
@@ -91,13 +114,10 @@ int maxValueIn(int arr[], const int n)
 	return maxValueIn;
 }
 
-int number_of_shifts;
 
-void shiftLeft(int arr[], const int n)
+void shiftLeft(int arr[], const int n, int number_of_shifts)
 {
-
-	int number_of_shifts;
-	for (int i = 0; i < number_of_shifts;i++)
+	for (int i = 0; i < number_of_shifts; i++)
 	{
 		int buffer = arr[0];
 		for (int i = 0; i < n; i++)
@@ -106,7 +126,53 @@ void shiftLeft(int arr[], const int n)
 		}
 		arr[n - 1] = buffer;
 	}
-	
-	
+}
 
+void shiftRight(int arr[], const int n, int number_of_shifts)
+{
+	shiftRight(arr, n, n - number_of_shifts);
+}
+
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i+1; j < n; j++)
+		{
+			
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+
+void FillRand(double arr[], const int n, int minRand, int maxRand)
+{
+	// заполненение случайными числами
+	if (minRand > maxRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	if (minRand == maxRand)maxRand++;
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % int((maxRand - minRand) + minRand);
+		arr[i] /= 100;
+	}
+}
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
--- Random/main.cpp
+++ Random/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>;
 using namespace std;
 //#define RANDOM_MIN_MAX
+//#define SORTING_ARRAY
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -25,32 +27,42 @@ void main()
 	cout << endl;
 #endif // RANDOM_MIN_MAX
 	
-	const int n = 50;
+#ifdef SORTING_ARRAY
+	const int n = 100;
 	int arr[n];
+	cout << "Массив: " << endl;
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
 	}
 	for (int i = 0; i < n; i++)
 	{
-			cout << arr[i] << "\t";
+		cout << arr[i] << "\t";
 	}
 	cout << endl;
-	
+
 	int temp;
 	for (int i = 0; i < n; i++)
 	{
-		temp = arr[i];
-		arr[i+1] = temp;
-		i++;
-		if (arr[i+1]>arr[i])
+		for (int j = 0; j < n; j++)
 		{
-			cout << "отсортированный массив"<<arr[i + 1] << "\t";
-			
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
 		}
-
 	}
 	cout << endl;
+	cout << "Массив после сортировки: " << endl;
+	for (int j = 0; j < n; j++)
+	{
+		cout << arr[j + 1] << "\t";
+	}
+	cout << endl;
+#endif // SORTING_ARRAY
+
 
 
 
