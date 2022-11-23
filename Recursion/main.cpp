593eb744a0e63ed2c68c2523d35aab8afa489326
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2022-11-23 17:20:12 +0300

    \\

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -2,14 +2,31 @@
 using namespace std;
 
 void elevator(int floor);
+long int Fact(long int N);
+long int Pow(long int Num, int stepen);
+long int Fib(long int N);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Recursion" << endl;
-	int n;
-	cout << "Введите номер этажа: "; cin >> n;
-	elevator(n);
+	//cout << "Hello Recursion" << endl;
+	//int n;
+	//cout << "Введите номер этажа: "; cin >> n;
+	//elevator(n);
+	long number;
+	int Num, stepen;
+	cout << "Введите число для вычисления факториала: "; cin >> number;
+	long result = Fact(number);
+	cout << "Факториал равен: " << result << "\n";
+	cout << "Введите число: "; cin >> Num; 
+	cout << "Введите степень "; cin >> stepen;
+	long value = Pow(Num,stepen);
+	cout << "Результат " << value << "\n";
+	const int N;
+	cout << "Введите предел для ряда Фибоначи: "; cin >> N;
+
+
+	
 }
 void elevator(int floor)
 {
@@ -21,4 +38,26 @@ void elevator(int floor)
 	cout << "Вы на " << floor << " этаже " << endl;
 	elevator(floor-1);
 	cout << "Вы на " << floor << " этаже " << endl;
+}
+long int Fact(long int N) 
+{
+	if (N == 1)
+		return 1;
+	else
+		return N * Fact(N - 1) ;
+	if (N < 1)
+		return 0;
+}
+
+// в работе
+long int Pow(long int Num,int stepen)
+{
+	if (stepen == 0)
+		return 1;
+	else
+		return Num * Pow(Num, stepen - 1);
+}
+long int Fib(long int N)
+{
+	return  Fib(N - 2) + Fib(N - 1);
 }
